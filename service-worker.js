69dc1013f188f8deda8ba6c8d828227b6aaeef41
Nicolas STEYER <nicolas.steyer@flams.local>
2026-07-17 13:36:34 +0200

    Refresh client entry branding and cache busting

--- service-worker.js
+++ service-worker.js
@@ -25,13 +25,14 @@ const APP_SHELL = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)));
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
